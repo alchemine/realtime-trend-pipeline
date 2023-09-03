c54381ee223233538728ea2cf396976f0f7967af
alchemine <djyoon0223@gmail.com>
2023-09-04 01:25:36 +0900

    [update] Key of message: 0 -> None

--- operators/request_message/request_message.py
+++ operators/request_message/request_message.py
@@ -2,12 +2,4 @@ from realtime_trend_pipeline.common import *
 
 
 def producer(text, output_path):
-    yield (
-        json.dumps(0),
-        json.dumps(
-            {
-                "text": text,
-                "output_path": output_path
-            }
-        )
-    )
+    yield (None, json.dumps({"text": text, "output_path": output_path}))
